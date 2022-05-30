775d9b275e916c55f20e6a974ba07b074680e4b4
kaceo <kaceong@gmail.com>
2022-05-30 15:53:36 +0800

    more

--- src/client/Layout.vue
+++ src/client/Layout.vue
@@ -2,9 +2,16 @@
 .main
   h1 Hello World
   hr
+
   Content
   hr
+  pre
+    | page: {{ page }}
+    | lang: {{ lang }}
+    | route: {{ route }}
+  hr
   .footer {{h}}
+
 </template>
 
 
@@ -13,6 +20,9 @@
 
 
 <script setup>
+import { useData, useRoute } from 'vitepress'
 import { ref } from 'vue'
 const h=ref("hello")
+const route=useRoute()
+const { page, lang }=useData()
 </script>
--- src/client/index.ts
+++ src/client/index.ts
@@ -2,11 +2,31 @@
 
 import myLayout from './Layout.vue'
 import defaultTheme from 'vitepress/dist/client/theme-default'
+//import { appendFile } from 'fs'
 
 export const vptheme = (callback)=>{
-  callback()
   return {
     ...defaultTheme,
-    //Layout: myLayout,
+    Layout: myLayout,
+    enhanceApp(ctx) {
+      let submodules = import.meta.globEager('~/components/**/*.vue')
+      register(ctx.app, submodules)
+      callback(ctx.app)
+    }
   }
+}
+
+
+
+/* Register components */
+function register(app, components) {
+  // install auto components under app/modules/
+  Object.entries(components).forEach(([path, definition]) => {
+    const _componentName = path.split('/').pop().replace(/\.\w+$/, '')
+    app.component(_componentName, definition.default)
+  })
+  // install runtime Dyn component
+  //app.component('Dyn', dyn)
+  // install data stores from app/stores/
+  //app.use(stores)
 }
